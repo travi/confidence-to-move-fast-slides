6941338412ed9def44c9116b372fbb0f969328b6
Matt Travi <programmer@travi.org>
2019-04-06 16:44:17 -0500

    feat(intro): added slides that set context

--- src/presentation.js
+++ src/presentation.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  Link,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text
-} from 'spectacle';
+import {Deck, Heading, Link, List, ListItem, Slide} from 'spectacle';
 import createTheme from 'spectacle/lib/themes/default';
 
 require('normalize.css');
@@ -29,33 +18,141 @@ export default function Presentation() {
         <Heading size={1} fit caps lineHeight={1} textColor="secondary">
           With Enough Confidence,
         </Heading>
-        <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold caps>
+        <Heading size={2} margin="10px 0 0" textColor="tertiary" fit bold caps>
           You Can Move Fast
-        </Text>
+        </Heading>
       </Slide>
-      <Slide transition={['fade']} bgColor="tertiary">
-        <Heading size={6} textColor="primary" caps>Typography</Heading>
-        <Heading size={1} textColor="secondary">Heading 1</Heading>
-        <Heading size={2} textColor="secondary">Heading 2</Heading>
-        <Heading size={3} textColor="secondary">Heading 3</Heading>
-        <Heading size={4} textColor="secondary">Heading 4</Heading>
-        <Heading size={5} textColor="secondary">Heading 5</Heading>
-        <Text size={6} textColor="secondary">Standard text</Text>
-      </Slide>
-      <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-        <Heading size={6} textColor="secondary" caps>Standard List</Heading>
-        <List>
-          <ListItem>Item 1</ListItem>
-          <ListItem>Item 2</ListItem>
-          <ListItem>Item 3</ListItem>
-          <ListItem>Item 4</ListItem>
-        </List>
-      </Slide>
+
       <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-        <BlockQuote>
-          <Quote>Example Quote</Quote>
-          <Cite>Author</Cite>
-        </BlockQuote>
+        <Heading size={1} fit caps lineHeight={1} textColor="primary">
+          Software
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Software
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          is meant to be Soft
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Soft Means
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          it should be easy to change
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Software Doesn&apos;t Truly Change
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Until It Is Delivered to Production
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering Changes to Production
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Can be Scary
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering Changes to Production
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Can be Dangerous
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering Changes to Production
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Introduces Risk
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Heading size={1} fit caps lineHeight={1} textColor="primary">
+          Risk
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering Changes to Production
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Needs to be Safe
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Many Options Exist
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          to Reduce Risk
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Traditionally, Reducing Risk Meant
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Minimizing the Frequency of Changing Production
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering to Production
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Required High Quality
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Delivering to Production Again
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Should Be Avoided
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="tertiary">
+        <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+          Ensuring Quality Meant
+        </Heading>
+        <Heading size={2} margin="10px 0 0" textColor="primary" fit bold caps>
+          Making Software Less Soft
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Heading size={1} fit caps lineHeight={1} textColor="primary">
+          Confidence
+        </Heading>
+      </Slide>
+
+      <Slide transition={['fade']} bgColor="secondary" textColor="primary">
+        <Heading size={1} fit caps lineHeight={1} textColor="primary">
+          Rewards
+        </Heading>
       </Slide>
 
       <Slide transition={['fade']} bgColor="tertiary" textColor="secondary">
